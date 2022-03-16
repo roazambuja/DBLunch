cc094b8db56cef8aa37eddcbe0eda2ec0feff2f9
Isis Gomes Fabiano Pereira <isis.pereira@dbserver.com.br>
2022-03-16 15:33:43 -0300

    Ajustes na validação de usuário

--- JS/validaUsuario.js
+++ JS/validaUsuario.js
@@ -1,36 +1,58 @@
-export function valida(input) {
-    const tipoDeInput = input.dataset.tipo
-
-    if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
-    }
-
-    if(input.validity.valid) {
-        input.parentElement.classList.remove('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+$(function () {
+    const inputs = $(".input__field");
+  
+    $.each(inputs, function (index, input) {
+      $(input).blur(function () {
+        valida(input);
+      });
+    });
+  });
+  
+  function valida(input) {
+    const tipoDeInput = $(input).data().tipo;
+  
+    let div = $(input).parent().get(0);
+    if (input.checkValidity()) {
+      $(div).removeClass("input__invalid");
+      $($(div).children(".input__error").get(0)).text("");
     } else {
-        input.parentElement.classList.add('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+      $(div).addClass("input__invalid");
+  
+      $($(div).children(".input__error").get(0)).text(
+        mostraMensagemDeErro(tipoDeInput, input)
+      );
     }
-}
-
-const tiposDeErro = [
-    'valueMissing',
-    'typeMismatch',
-    'patternMismatch',
-    'customError'
-]
-
-const mensagensDeErro = {
+  }
+  
+  function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = "";
+  
+    $.each(tiposDeErro, function (index, erro) {
+      if (input.validity[erro]) {
+        mensagem = mensagensDeErro[tipoDeInput][erro];
+      }
+    });
+    return mensagem;
+  }
+  
+  const tiposDeErro = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+  ];
+  
+  const mensagensDeErro = {
     nome: {
-        valueMissing: 'O campo de nome não pode estar vazio.'
+      valueMissing: "O campo de nome não pode estar vazio.",
     },
     email: {
-        valueMissing: 'O campo de email não pode estar vazio.',
-        typeMismatch: 'O email digitado não é válido.'
+      valueMissing: "O campo de email não pode estar vazio.",
+      typeMismatch: "O email digitado não é válido.",
     },
     senha: {
-        valueMissing: 'O campo de senha não pode estar vazio.',
-        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+      valueMissing: "O campo de senha não pode estar vazio.",
+      patternMismatch:
+        "A senha deve conter entre 8 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.",
     },
-}
+  };
