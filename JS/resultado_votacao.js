2db4e900d1d3702f9505404f3955bb96b003405b
Isis Gomes Fabiano Pereira <isis.pereira@dbserver.com.br>
2022-03-21 11:00:53 -0300

    Criando e configurando tela do resultado da votação

---
+++ JS/resultado_votacao.js
@@ -0,0 +1,20 @@
+import { restaurantes } from "./restaurantes.js";
+
+$(function () {
+  let maisVotado = 0;
+  let vencedor;
+  restaurantes.map((restaurante) => {
+    if (restaurante.votos > maisVotado) {
+      maisVotado = restaurante.votos;
+      vencedor = restaurante;
+    }
+  });
+  console.log(maisVotado);
+  console.log(vencedor.nome);
+  var mostraVencedor = $("<a class='ganhadorVotacao'/>");
+  $(mostraVencedor).text(vencedor.nome);
+  if (vencedor.site) {
+    $(mostraVencedor).attr("href", vencedor.site);
+  }
+  $(".vencedorVotacao").append(mostraVencedor);
+});
